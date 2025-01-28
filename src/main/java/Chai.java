36f7c46b81a85bac618db7a98c062d04d32b176f
Jeromy <jeromytann@gmail.com>
2025-01-28 16:43:17 +0800

    Updated Chai with List function

--- src/main/java/Chai.java
+++ src/main/java/Chai.java
@@ -1,9 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Chai {
+    static String[] list = new String[100];
+    static int listIndex = 0;
+
+    public static void getList() {
+        System.out.println("____________________________________________________________");
+        for (int i = 0; i < listIndex; i++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
     public static void echo(String message) {
         System.out.println("____________________________________________________________\n" +
-                message + "\n" +
+                "added: " + message + "\n" +
                 "____________________________________________________________\n");
     }
 
@@ -17,13 +29,12 @@ public class Chai {
                 " ||  |`-'| |  .-.  |  |  .-.  | ,|  |_.' \n" +
                 " (_'  '--'\\ |  | |  |  |  | |  |(_|  |    \n" +
                 "   `-----' `--' `--'  `--' `--'  `--'    \n";
+        Scanner in = new Scanner(System.in);
+        String input;
 
         System.out.println("____________________________________________________________\n" +
                 "Hello! I'm Chai\n" + logo + "What can I do for you?\n" +
                 "____________________________________________________________");
-
-        Scanner in = new Scanner(System.in);
-        String input;
 
         while (true) {
             input = in.nextLine();
@@ -32,8 +43,19 @@ public class Chai {
                         "Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                getList();
+            } else {
+                if (listIndex >= list.length) {
+                    System.out.println("__________________________________________________________\n" +
+                             "List is full!\n" +
+                            "______________________________________________________________");
+                } else {
+                    list[listIndex] = input;
+                    echo(list[listIndex]);
+                    listIndex++;
+                }
             }
-            echo(input);
         }
         in.close();
     }
